5b7667d73072077a3a5f1dd433181afe49874255
panjinjie <panjjo@panjinjies-iMac.local>
2018-09-13 18:17:43 +0800

    add log

--- fly/calculate.go
+++ fly/calculate.go
@@ -10,6 +10,7 @@ type Calculation struct {
 
 func (c *Calculation) reader(d *BodyData) {
 	t := instance.TMP{B: d.Body, Tag: d.Tag, WG: &sync.WaitGroup{}}
+	log.TRACE.Printf("receive op:%d,tag:%s,data:%s", d.Op, d.Tag, string(d.Body))
 	code := t.EXEC()
 	if d.NeedResp != 0 {
 		ConnRespChannel <- &connResp{d.Connid, code, nil}
--- fly/stat.go
+++ fly/stat.go
@@ -6,11 +6,12 @@ type Statistics struct {
 }
 
 func (s *Statistics) reader(d *BodyData) {
-
+	log.TRACE.Printf("receive connid:%s, op:%d,tag:%s,data:%s", d.Connid, d.Op, d.Tag, string(d.Body))
 	err, result := snow.Stat(d.Body, d.Tag)
 	if err != nil {
 		log.ERROR.Printf("Stat error tag:%s,err:%s", d.Tag, err)
 	}
+	log.TRACE.Printf("response connid:%s, op:%d,tag:%s,data:%s", d.Connid, d.Op, d.Tag, string(d.Body))
 	ConnRespChannel <- &connResp{d.Connid, 0, result}
 }
 
@@ -18,10 +19,12 @@ type Clear struct {
 }
 
 func (s *Clear) reader(d *BodyData) {
+	log.TRACE.Printf("response connid:%s, op:%d,tag:%s,data:%s", d.Connid, d.Op, d.Tag, string(d.Body))
 	err, result := snow.Clear(d.Body)
 	if err != nil {
 		log.ERROR.Printf("Clear error err:%s", err)
 	}
+	log.TRACE.Printf("response connid:%s, op:%d,tag:%s,data:%s", d.Connid, d.Op, d.Tag, string(d.Body))
 	ConnRespChannel <- &connResp{d.Connid, result, err}
 }
 func ClearRedisKey(tag string) {
